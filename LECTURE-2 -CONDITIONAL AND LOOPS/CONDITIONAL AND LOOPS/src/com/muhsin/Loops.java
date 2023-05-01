a53bd29c99aa564ac2131c868f0c0cac0534c3a6
Muhsin <badboy101011@gmail.com>
2023-05-01 20:05:29 +0530

    conditional and loops

--- LECTURE-2 -CONDITIONAL AND LOOPS/CONDITIONAL AND LOOPS/src/com/muhsin/Loops.java
+++ LECTURE-2 -CONDITIONAL AND LOOPS/CONDITIONAL AND LOOPS/src/com/muhsin/Loops.java
@@ -9,15 +9,19 @@ public class Loops {
         //int n = in.nextInt();
 
 
-       // for( int num = 1 ; num <=5 ; num +=1){
-            //System.out.println(num);
+        // for( int num = 1 ; num <=5 ; num +=1){
+        //System.out.println(num);
 
-        int num = 1;
-        while (num <=5 ){
-            System.out.println(num);
-            num +=1;
-        }
-
+//        int num = 1;
+//        while (num <=5 ){
+//            System.out.println(num);
+//            num +=1;
+        int n = 1;
+        do {
+            System.out.println(n);
+            n++;
+        } while (n <=5);
 
     }
 }
+
