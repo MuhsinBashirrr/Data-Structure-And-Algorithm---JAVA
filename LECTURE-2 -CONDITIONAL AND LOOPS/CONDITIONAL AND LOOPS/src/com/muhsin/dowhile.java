d538acb0df5773eb730dfef6c8bae168c4287c67
Muhsin <badboy101011@gmail.com>
2023-05-01 20:09:28 +0530

    conditional and loops

---
+++ LECTURE-2 -CONDITIONAL AND LOOPS/CONDITIONAL AND LOOPS/src/com/muhsin/dowhile.java
@@ -0,0 +1,16 @@
+package com.muhsin;
+import java.util.Scanner;
+
+public class dowhile {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = 1;
+        do {
+            System.out.println(n);
+            n++;
+        } while (n <=5);
+
+    }
+}
+
+
