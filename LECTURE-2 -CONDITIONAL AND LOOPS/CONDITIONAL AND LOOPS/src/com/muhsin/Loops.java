cf00c7c14b80ed6c212bc334f4c8c41349ddbef3
Muhsin <badboy101011@gmail.com>
2023-05-01 19:57:42 +0530

    conditional and loops

---
+++ LECTURE-2 -CONDITIONAL AND LOOPS/CONDITIONAL AND LOOPS/src/com/muhsin/Loops.java
@@ -0,0 +1,23 @@
+package com.muhsin;
+
+import java.util.Enumeration;
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //int n = in.nextInt();
+
+
+       // for( int num = 1 ; num <=5 ; num +=1){
+            //System.out.println(num);
+
+        int num = 1;
+        while (num <=5 ){
+            System.out.println(num);
+            num +=1;
+        }
+
+
+    }
+}
