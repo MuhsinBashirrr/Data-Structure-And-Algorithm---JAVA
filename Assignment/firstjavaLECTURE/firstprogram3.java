6d34cc462994de5614c18d74760ff699c78223f8
Muhsin Bashir <101365717+MuhsinBashirrr@users.noreply.github.com>
2023-05-04 22:48:43 +0530

    Add files via upload

---
+++ Assignment/firstjavaLECTURE/firstprogram3.java
@@ -0,0 +1,23 @@
+package firstjavaLECTURE;
+
+import java.util.Scanner;
+
+public class firstprogram3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the principal amount: ");
+        double principal = input.nextDouble();
+
+        System.out.print("Enter the time (in years): ");
+        double time = input.nextDouble();
+
+        System.out.print("Enter the rate of interest: ");
+        double rate = input.nextDouble();
+
+        // Calculate the simple interest
+        double simpleInterest = (principal * time * rate) / 100;
+
+        // Display the result
+        System.out.println("Simple interest = " + simpleInterest);
+    }
+}
