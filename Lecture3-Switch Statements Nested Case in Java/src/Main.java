46451c9a5ecd36cc1890836c64558ae3e466d0b4
Muhsin <badboy101011@gmail.com>
2023-05-05 20:03:33 +0530

    enhanced switch

--- Lecture3-Switch Statements Nested Case in Java/src/Main.java
+++ Lecture3-Switch Statements Nested Case in Java/src/Main.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        String fruit = input.next();
+        int day = input.nextInt();
 
-        switch (fruit) {
-            case "Mango" -> System.out.println("King of fruits");
-            case "Apple" -> System.out.println("Red in color");
-            case "Orange" -> System.out.println(" A round fruit");
-            case "Grapes" -> System.out.println(" A small fruit");
-            default -> System.out.println("Please enter a valid fruit");
+        switch (day) {
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println(" Wednesday");
+            case 4 -> System.out.println(" Thursday");
+            default -> System.out.println("Please enter a valid day");
         }
 
 
